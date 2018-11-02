928e09686f069c3690bfb6faa1cda2bbf5b083fd
Abhishek <lohaniabhishek@gmail.com>
2018-11-02 11:51:37 +0530

    Update jsC8.ts

--- src/jsC8.ts
+++ src/jsC8.ts
@@ -3,6 +3,8 @@ import { CollectionType } from "./collection";
 import { Config } from "./connection";
 import { Fabric } from "./fabric";
 import { C8Error } from "./error";
+import { Stream } from "./stream";
+import { Tenant } from "./tenant";
 
 export default function jsC8(config: Config) {
   return new Fabric(config);
@@ -11,4 +13,4 @@ export default function jsC8(config: Config) {
 Object.assign(jsC8, { CollectionType, C8Error, Fabric, c8ql });
 export { DocumentCollection, EdgeCollection } from "./collection";
 export { Graph } from "./graph";
-export { Fabric, c8ql };
+export { Fabric, c8ql, Tenant, Stream };
