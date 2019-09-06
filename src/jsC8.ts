aeeecbbe5ee5241a58172127f945d90337f34d3d
rachitpsolanki <rachitpsolanki@gmail.com>
2019-09-06 20:54:51 +0530

    Added exports for collection and cursor

--- src/jsC8.ts
+++ src/jsC8.ts
@@ -1,8 +1,9 @@
 import { c8ql } from "./c8ql-query";
-import { CollectionType } from "./collection";
+import { CollectionType, BaseCollection } from "./collection";
 import { Config } from "./connection";
 import { Fabric } from "./fabric";
 import { C8Error } from "./error";
+import { ArrayCursor } from "./cursor";
 
 export default function jsC8(config: Config) {
   return new Fabric(config);
@@ -14,3 +15,4 @@ export { Graph } from "./graph";
 export { Tenant } from "./tenant";
 export { Stream } from "./stream";
 export { Fabric, c8ql };
+export { Config, BaseCollection, ArrayCursor };
