71fde7750a3693b7a45efde042af53ab8413d49b
Nandha <nandha2447@gmail.com>
2019-09-08 23:06:08 +0530

    review comment changes and markdown

--- src/test/32-pipelines.ts
+++ src/test/32-pipelines.ts
@@ -1,12 +1,13 @@
 import { expect } from "chai";
 import { Fabric } from "../jsC8";
+import { getDCListString } from "../util/helper";
 
 describe("Manipulating pipelines", function() {
     this.timeout(50000);
   
     let fabric: Fabric;
     const testUrl: string =
-      process.env.TEST_C8_URL || "https://qa2.eng1.macrometa.io";
+      process.env.TEST_C8_URL || "https://test.macrometa.io";
   
     before(async () => {
       fabric = new Fabric({
@@ -16,7 +17,6 @@ describe("Manipulating pipelines", function() {
       await fabric.login("guest", "root", "guest");
       fabric.useTenant("guest");
       fabric.useFabric("_system");
-          
     });
   
     after(() => {
@@ -25,19 +25,20 @@ describe("Manipulating pipelines", function() {
   
     describe("fabric.getAllPipelines", () => {
       it("returns an empty array or an array of all pipelines", async() => {
-        const regions = ["qa2-asia-east1"];
+        const dcList = await fabric.getAllEdgeLocations();
+        const regions = getDCListString(dcList).split(',');
         const isEnabled = false;
         const config = {
           "input": {
           "type": "c8db",
           "c8db": {
-            "name": "collection_name1"
+            "name": "collection_namepl1"
           }
         },
           "output": {
           "type": "c8streams",
           "c8streams": {
-            "name": "stream_name1",
+            "name": "stream_namepl1",
             "local": true
           }
         }
@@ -45,7 +46,7 @@ describe("Manipulating pipelines", function() {
         const allPipelines = await fabric.getPipelines();
         const pipelinesCount = allPipelines.length;
         expect(allPipelines).to.be.an('array');
-        const pipeline = await fabric.pipeline('pipeline1');
+        const pipeline = fabric.pipeline('testPipeline');
         await pipeline.create(regions,isEnabled, config);
         const updatedAllPipelines = await fabric.getPipelines();
         const expectedPipelinesCount = pipelinesCount + 1;
@@ -55,24 +56,25 @@ describe("Manipulating pipelines", function() {
   
     describe("pipeline.create", () => {
         it("creates a new pipeline", async () => {
-            const regions = ["qa2-asia-east1"];
+            const dcList = await fabric.getAllEdgeLocations();
+            const regions = getDCListString(dcList).split(',');
             const isEnabled = false;
             const config = {
                 "input": {
                     "type": "c8db",
                     "c8db": {
-                        "name": "collection_name2"
+                        "name": "collection_namepl2"
                     }
                     },
                 "output": {
                     "type": "c8streams",
                     "c8streams": {
-                        "name": "stream_name2",
+                        "name": "stream_namepl2",
                         "local": true
                     }
                 }
             };
-            const pipeline = await fabric.pipeline('pipeline2');
+            const pipeline = fabric.pipeline('testPipeline2');
             const response = await pipeline.create(regions,isEnabled, config);
             expect(response).to.equal('Stream Created Successfully');
         });
@@ -83,7 +85,7 @@ describe("Manipulating pipelines", function() {
             const allPipelines = await fabric.getPipelines();
             const pipelineName = allPipelines[0].name;
             const pipelinesCount = allPipelines.length;
-            const pipeline = await fabric.pipeline(pipelineName);
+            const pipeline = fabric.pipeline(pipelineName);
             await pipeline.drop();
             const updatedPipelines = await fabric.getPipelines();
             const updatedPipelinesCount = updatedPipelines.length;
@@ -96,7 +98,7 @@ describe("Manipulating pipelines", function() {
         it("fetches the details of an existing pipeline", async () => {
             const allPipelines = await fabric.getPipelines();
             const pipelineName = allPipelines[0].name;
-            const pipeline = await fabric.pipeline(pipelineName);
+            const pipeline = fabric.pipeline(pipelineName);
             const response = await pipeline.details();            
             expect(response).to.be.an('object');
             expect(response).to.have.property('name');
@@ -110,16 +112,16 @@ describe("Manipulating pipelines", function() {
         it("updates an existing pipeline", async () => {
             const allPipelines = await fabric.getPipelines();
             const pipelineName = allPipelines[0].name;
-            const pipeline = await fabric.pipeline(pipelineName);
+            const pipeline = fabric.pipeline(pipelineName);
             const response = await pipeline.details();
             const { regions, enabled: isEnabled, config } = response;
-            const enabled = !isEnabled;
-            const updatedMessage = await pipeline.update(regions, enabled, config);
+            const toggleEnabled = !isEnabled;
+            const updatedMessage = await pipeline.update(regions, toggleEnabled, config);
             expect(updatedMessage).to.equal('Stream Updated Successfully');
-            const updatedPipeline = await fabric.pipeline(pipelineName);
+            const updatedPipeline = fabric.pipeline(pipelineName);
             const { enabled: updatedEnabledValue } = await updatedPipeline.details();
             expect(updatedEnabledValue).to.not.equal(isEnabled);
-            expect(updatedEnabledValue).to.equal(enabled);
+            expect(updatedEnabledValue).to.equal(toggleEnabled);
         });
     });
 });
