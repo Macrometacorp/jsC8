a1422e224b27b0af447a054c9bd5e42e7ab5184a
Abhishek <abhishek.lohani@macrometa.co>
2020-02-28 11:45:42 +0530

    resolved merge conflict

--- src/fabric.ts
+++ src/fabric.ts
@@ -755,22 +755,6 @@ export class Fabric {
 
   createStreamApp(regions: Array<string>, appDefinition: string): Promise<any> {
     return this._connection.request(
-<<<<<<< HEAD
-        {
-            method: "POST",
-            path: "/_api/streamapps",
-            body: JSON.stringify({
-                "definition": appDefinition, 
-                "regions": regions,
-            })
-        },
-
-        res => res.body
-    )
-
-
-}
-=======
       {
         method: "POST",
         path: "/_api/streamapps",
@@ -782,7 +766,6 @@ export class Fabric {
       res => res.body
     );
   }
->>>>>>> fabac51dee56054a458399600932b1c688f0d721
 
   getAllStreamApps() {
     return this._connection.request(
