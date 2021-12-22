5555c141c69049198eccfc8bdee6b00c61d124b2
ajaykrishna.n <ajay.krishna@macrometa.com>
2021-12-22 12:33:33 +0530

    [JSDR-36] Remove csvtojson and use papaparser

---
+++ src/util/parseCsv.ts
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const papaParser = require('papaparse');
+
+// Added "File" here so that the function does't complain, as by default the types are being assumed for node
+export function parseCSVToJSON(csvPath: string | File) {
+  if (typeof csvPath !== "string") {
+    throw new Error("Please provide valid path");
+  }
+
+  return new Promise((resolve, reject) => {
+    const file = fs.readFileSync(csvPath, { encoding: 'utf8', flag: 'r' });
+    const { data, errors } = papaParser.parse(file, {
+      header: true,
+    });
+    if (errors.length) {
+      reject(errors);
+    } else {
+      resolve(data);
+    }
+  });
+}
