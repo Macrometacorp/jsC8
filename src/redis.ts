44cc9690fb5229e9da1a19e88b4eabcbd9ad8d55
Dino Lozina <dino.lozina@macrometa.com>
2022-11-21 17:17:25 +0100

    Added all redis list commands

--- src/redis.ts
+++ src/redis.ts
@@ -7,11 +7,6 @@ export class Redis {
     this._connection = new Connection(config);
   }
 
-  set(key: string, value: string, collection: string, options: any[] = []) {
-    const command: string = "SET";
-    return this._commandParser(command, collection, key, value, ...options);
-  }
-
   protected _commandParser(
     command: string,
     collection: string,
@@ -19,15 +14,366 @@ export class Redis {
   ) {
     // Create a body for all the commands
     const data = [command, ...args];
+    const filteredData = data.filter(element => {
+      return element !== undefined;
+    });
     let request = this._connection.request(
       {
         method: "POST",
         path: `/_api/redis/${collection}`,
-        body: data,
+        body: filteredData,
       },
       res => res.body
     );
 
     return request;
+  }
+
+  set(key: string, value: string, collection: string, options: any[] = []) {
+    const command: string = "SET";
+    return this._commandParser(command, collection, key, value, ...options);
+  }
+
+  append(key: string, value: string, collection: string) {
+    const command: string = "APPEND";
+    return this._commandParser(command, collection, key, value);
+  }
+
+  decr(key: string, collection: string) {
+    const command: string = "DECR";
+    return this._commandParser(command, collection, key);
+  }
+
+  decrby(key: string, decrement: number, collection: string) {
+    const command: string = "DECRBY";
+    return this._commandParser(command, collection, key, decrement);
+  }
+
+  get(key: string, collection: string) {
+    const command: string = "GET";
+    return this._commandParser(command, collection, key);
+  }
+
+  getdel(key: string, collection: string) {
+    const command: string = "GETDEL";
+    return this._commandParser(command, collection, key);
+  }
+
+  getex(
+    key: string,
+    collection: string,
+    expiryCommand?: string,
+    time?: number
+  ) {
+    const command: string = "GETEX";
+    return this._commandParser(command, collection, key, expiryCommand, time);
+  }
+
+  getrange(key: string, start: number, end: number, collection: string) {
+    const command: string = "GETRANGE";
+    return this._commandParser(command, collection, key, start, end);
+  }
+
+  getset(key: string, value: string, collection: string) {
+    const command: string = "GETSET";
+    return this._commandParser(command, collection, key, value);
+  }
+
+  incr(key: string, collection: string) {
+    const command: string = "INCR";
+    return this._commandParser(command, collection, key);
+  }
+
+  incrby(key: string, increment: number, collection: string) {
+    const command: string = "INCRBY";
+    return this._commandParser(command, collection, key, increment);
+  }
+
+  incrbyfloat(key: string, increment: number, collection: string) {
+    const command: string = "INCRBYFLOAT";
+    return this._commandParser(command, collection, key, increment);
+  }
+
+  mget(keys: string[], collection: string) {
+    const command: string = "MGET";
+    return this._commandParser(command, collection, ...keys);
+  }
+
+  mset(data: object, collection: string) {
+    const dataArray: string[] = [];
+    for (const [key, value] of Object.entries(data)) {
+      dataArray.push(key);
+      dataArray.push(value);
+    }
+    const command: string = "MSET";
+    return this._commandParser(command, collection, ...dataArray);
+  }
+
+  psetex(key: string, milliseconds: number, value: number, collection: string) {
+    const command: string = "PSETEX";
+    return this._commandParser(command, collection, key, milliseconds, value);
+  }
+
+  setbit(key: string, offset: number, value: number, collection: string) {
+    const command: string = "SETBIT";
+    return this._commandParser(command, collection, key, offset, value);
+  }
+
+  msetnx(data: object, collection: string) {
+    const dataArray: string[] = [];
+    for (const [key, value] of Object.entries(data)) {
+      dataArray.push(key);
+      dataArray.push(value);
+    }
+    const command: string = "MSETNX";
+    return this._commandParser(command, collection, ...dataArray);
+  }
+
+  setex(key: string, seconds: number, value: string, collection: string) {
+    const command: string = "SETEX";
+    return this._commandParser(command, collection, key, seconds, value);
+  }
+
+  setnx(key: string, value: string, collection: string) {
+    const command: string = "SETNX";
+    return this._commandParser(command, collection, key, value);
+  }
+
+  setrange(key: string, offset: number, value: string, collection: string) {
+    const command: string = "SETRANGE";
+    return this._commandParser(command, collection, key, offset, value);
+  }
+
+  strlen(key: string, collection: string) {
+    const command: string = "STRLEN";
+    return this._commandParser(command, collection, key);
+  }
+
+  bitcount(
+    key: string,
+    collection: string,
+    start?: number | undefined,
+    end?: number | undefined,
+    dataFormat?: string | undefined
+  ) {
+    const command: string = "BITCOUNT";
+    return this._commandParser(
+      command,
+      collection,
+      key,
+      start,
+      end,
+      dataFormat
+    );
+  }
+
+  bittop(
+    operation: string,
+    deskey: string,
+    keys: string[],
+    collection: string
+  ) {
+    const command: string = "BITOP";
+    return this._commandParser(command, collection, operation, deskey, ...keys);
+  }
+
+  bitpos(
+    key: string,
+    bit: number,
+    collection: string,
+    start?: number | undefined,
+    end?: number | undefined,
+    dataFormat?: string | undefined
+  ) {
+    const command: string = "BITPOS";
+    return this._commandParser(
+      command,
+      collection,
+      key,
+      bit,
+      start,
+      end,
+      dataFormat
+    );
+  }
+
+  getbit(key: string, offset: number, collection: string) {
+    const command: string = "GETBIT";
+    return this._commandParser(command, collection, key, offset);
+  }
+
+  lpush(key: string, elements: string[], collection: string) {
+    const command: string = "LPUSH";
+    return this._commandParser(command, collection, key, ...elements);
+  }
+
+  lindex(key: string, index: number, collection: string) {
+    const command: string = "LINDEX";
+    return this._commandParser(command, collection, key, index);
+  }
+
+  linsert(
+    key: string,
+    modifier: string,
+    pivot: string,
+    element: string,
+    collection: string
+  ) {
+    const command: string = "LINSERT";
+    return this._commandParser(
+      command,
+      collection,
+      key,
+      modifier,
+      pivot,
+      element
+    );
+  }
+
+  llen(key: string, collection: string) {
+    const command: string = "LLEN";
+    return this._commandParser(command, collection, key);
+  }
+
+  lrange(key: string, start: number, stop: number, collection: string) {
+    const command: string = "LRANGE";
+    return this._commandParser(command, collection, key, start, stop);
+  }
+
+  lmove(
+    source: string,
+    destination: string,
+    whereFrom: string,
+    whereTo: string,
+    collection: string
+  ) {
+    const command: string = "LMOVE";
+    return this._commandParser(
+      command,
+      collection,
+      source,
+      destination,
+      whereFrom,
+      whereTo
+    );
+  }
+
+  lpos(
+    key: string,
+    element: string,
+    collection: string,
+    rank?: number | undefined,
+    count?: number | undefined,
+    maxLen?: number | undefined
+  ) {
+    const command: string = "LPOS";
+
+    let rankArray: any[] = [];
+    if (rank !== undefined) {
+      rankArray.push("RANK");
+      rankArray.push(rank);
+    }
+
+    let countArray: any[] = [];
+    if (count !== undefined) {
+      countArray.push("COUNT");
+      countArray.push(count);
+    }
+
+    let maxLenArray: any[] = [];
+    if (maxLen !== undefined) {
+      maxLenArray.push("MAXLEN");
+      maxLenArray.push(maxLen);
+    }
+
+    return this._commandParser(
+      command,
+      collection,
+      key,
+      element,
+      ...rankArray,
+      ...countArray,
+      ...maxLenArray
+    );
+  }
+
+  rpush(key: string, elements: string[], collection: string) {
+    const command: string = "RPUSH";
+    return this._commandParser(command, collection, key, ...elements);
+  }
+
+  lpop(key: string, collection: string, count?: number | undefined) {
+    const command: string = "LPOP";
+    return this._commandParser(command, collection, key, count);
+  }
+
+  lpushx(key: string, elements: string[], collection: string) {
+    const command: string = "LPUSHX";
+    return this._commandParser(command, collection, key, ...elements);
+  }
+
+  rpushx(key: string, elements: string[], collection: string) {
+    const command: string = "RPUSHX";
+    return this._commandParser(command, collection, key, ...elements);
+  }
+
+  lrem(key: string, count: number, element: string, collection: string) {
+    const command: string = "LREM";
+    return this._commandParser(command, collection, key, count, element);
+  }
+
+  lset(key: string, index: number, element: string, collection: string) {
+    const command: string = "LSET";
+    return this._commandParser(command, collection, key, index, element);
+  }
+
+  ltrim(key: string, start: number, stop: number, collection: string) {
+    const command: string = "LTRIM";
+    return this._commandParser(command, collection, key, start, stop);
+  }
+
+  rpop(key: string, collection: string, count?: Number | undefined) {
+    const command: string = "RPOP";
+    return this._commandParser(command, collection, key, count);
+  }
+
+  rpoplpush(source: string, destination: string, collection: string) {
+    const command: string = "RPOPLPUSH";
+    return this._commandParser(command, collection, source, destination);
+  }
+
+  hset(key: string, data: object, collection: string) {
+    const dataArray: string[] = [];
+    for (const [key, value] of Object.entries(data)) {
+      dataArray.push(key);
+      dataArray.push(value);
+    }
+    const command: string = "HSET";
+    return this._commandParser(command, collection, key, ...dataArray);
+  }
+
+  hget(key: string, field: string, collection: string) {
+    const command: string = "HGET";
+    return this._commandParser(command, collection, key, field);
+  }
+
+  hdel(key: string, fields: string[], collection: string) {
+    const command: string = "HDEL";
+    return this._commandParser(command, collection, key, ...fields);
+  }
+
+  hexists(key: string, field: string, collection: string) {
+    const command: string = "HEXISTS";
+    return this._commandParser(command, collection, key, field);
+  }
+
+  hgetall(key: string, collection: string) {
+    const command: string = "HGETALL";
+    return this._commandParser(command, collection, key);
+  }
+
+  hincrby(key: string, field: string, increment: number, collection: string) {
+    const command: string = "HINCRBY";
+    return this._commandParser(command, collection, key, field, increment);
   }
 }
--- src/test/41-redis.ts
+++ src/test/41-redis.ts
@@ -9,7 +9,7 @@ describe("validating redis apis", function() {
   this.timeout(60000);
   let c8Client: C8Client;
 
-  beforeEach(async () => {
+  before(async () => {
     c8Client = new C8Client({
       url: process.env.URL,
       apiKey: process.env.API_KEY,
@@ -20,20 +20,429 @@ describe("validating redis apis", function() {
 
   describe("Redis", () => {
     const collectionName = "testRedisCollection";
-    beforeEach(async () => {
+    before(async () => {
       await c8Client.createCollection(collectionName);
     });
 
-    afterEach(async () => {
+    after(async () => {
       await c8Client.deleteCollection(collectionName);
     });
 
     describe("test redis string commands", () => {
       it("redis.set", async () => {
-        const response = await c8Client.redis.set("testKey", "testValue", collectionName);
+        const response = await c8Client.redis.set(
+          "testKey",
+          "1",
+          collectionName
+        );
         expect(response.result).to.equal("OK");
         expect(response.code).to.equal(200);
       });
+      it("redis.append", async () => {
+        const response = await c8Client.redis.append(
+          "testKey",
+          "2",
+          collectionName
+        );
+        expect(response.result).to.equal(2);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.dec", async () => {
+        const response = await c8Client.redis.decr("testKey", collectionName);
+        expect(response.result).to.equal(11);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.decby", async () => {
+        const response = await c8Client.redis.decrby(
+          "testKey",
+          10,
+          collectionName
+        );
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.get", async () => {
+        const response = await c8Client.redis.get("testKey", collectionName);
+        expect(response.result).to.equal("1");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.getdel", async () => {
+        const response = await c8Client.redis.getdel("testKey", collectionName);
+        expect(response.result).to.equal("1");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.getex", async () => {
+        await c8Client.redis.set("testKeyEx", "EX", collectionName);
+        const response = await c8Client.redis.getex(
+          "testKeyEx",
+          collectionName,
+          "EX",
+          200
+        );
+        expect(response.result).to.equal("EX");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.getrange", async () => {
+        const response = await c8Client.redis.getrange(
+          "testKeyEx",
+          0,
+          0,
+          collectionName
+        );
+        expect(response.result).to.equal("E");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.getset", async () => {
+        const response = await c8Client.redis.getset(
+          "testKeyEx",
+          "testValue",
+          collectionName
+        );
+        expect(response.result).to.equal("EX");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.incr", async () => {
+        const response = await c8Client.redis.incr("testKeyEx", collectionName);
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.incrby", async () => {
+        const response = await c8Client.redis.incrby(
+          "testKeyEx",
+          10,
+          collectionName
+        );
+        expect(response.result).to.equal(11);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.incrbyfloat", async () => {
+        const response = await c8Client.redis.incrbyfloat(
+          "testKeyEx",
+          0.5,
+          collectionName
+        );
+        expect(response.result).to.equal("11.5");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.mget", async () => {
+        await c8Client.redis.set("anotherTest", "22", collectionName);
+        const response = await c8Client.redis.mget(
+          ["testKeyEx", "anotherTest"],
+          collectionName
+        );
+        expect(response.result).to.eql(["11.5", "22"]);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.mset", async () => {
+        const response = await c8Client.redis.mset(
+          { testKey3: "testValue3", testKey4: "testValue4" },
+          collectionName
+        );
+        expect(response.result).to.equal("OK");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.msetnx", async () => {
+        const response = await c8Client.redis.msetnx(
+          { testKey5: "testValue5", testKey6: "testValue6" },
+          collectionName
+        );
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.setex", async () => {
+        const response = await c8Client.redis.setex(
+          "ttlKeySec",
+          30,
+          "value",
+          collectionName
+        );
+        expect(response.result).to.equal("OK");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.psetex", async () => {
+        const response = await c8Client.redis.setex(
+          "ttlKeyMs",
+          30000,
+          "value",
+          collectionName
+        );
+        expect(response.result).to.equal("OK");
+        expect(response.code).to.equal(200);
+      });
+      it("redis.setbit", async () => {
+        const response = await c8Client.redis.setbit(
+          "bitKey",
+          7,
+          0,
+          collectionName
+        );
+        expect(response.result).to.equal(0);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.setnx", async () => {
+        const response = await c8Client.redis.setnx(
+          "testSetNx",
+          "1",
+          collectionName
+        );
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.setrange", async () => {
+        const response = await c8Client.redis.setrange(
+          "testSetNx",
+          0,
+          "2",
+          collectionName
+        );
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.strlen", async () => {
+        await c8Client.redis.setnx("testString", "string", collectionName);
+        const response = await c8Client.redis.strlen(
+          "testString",
+          collectionName
+        );
+        expect(response.result).to.equal(6);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.bitcount", async () => {
+        await c8Client.redis.setnx("myKeyString", "foobar", collectionName);
+        const response = await c8Client.redis.bitcount(
+          "myKeyString",
+          collectionName
+        );
+        expect(response.result).to.equal(26);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.bitcount2", async () => {
+        const response = await c8Client.redis.bitcount(
+          "myKeyString",
+          collectionName,
+          0,
+          0
+        );
+        expect(response.result).to.equal(4);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.bittop", async () => {
+        await c8Client.redis.set("key1", "foobar", collectionName);
+        await c8Client.redis.set("key2", "abcdef", collectionName);
+        const response = await c8Client.redis.bittop(
+          "AND",
+          "dest",
+          ["key1", "key2"],
+          collectionName
+        );
+        expect(response.result).to.equal(6);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.getbit", async () => {
+        await c8Client.redis.setbit("mykey", 7, 1, collectionName);
+        const response = await c8Client.redis.getbit(
+          "mykey",
+          7,
+          collectionName
+        );
+        expect(response.result).to.equal(1);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.bitpos", async () => {
+        await c8Client.redis.set("mykey2", "\x00\x00\x00", collectionName);
+        const response = await c8Client.redis.bitpos(
+          "mykey2",
+          0,
+          collectionName
+        );
+        expect(response.result).to.equal(0);
+        expect(response.code).to.equal(200);
+      });
+      it("redis.bitpos2", async () => {
+        const response = await c8Client.redis.bitpos(
+          "mykey2",
+          1,
+          collectionName
+        );
+        expect(response.result).to.equal(-1);
+        expect(response.code).to.equal(200);
+      });
     });
+    describe("test redis list commands", () => {
+      it("redis.lpush", async () => {
+        const arrayData = ["iron", "gold", "copper"];
+        const response = await c8Client.redis.lpush(
+          "list",
+          arrayData,
+          collectionName
+        );
+        expect(response.result).to.equal(3);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lindex", async () => {
+        const response = await c8Client.redis.lindex("list", 0, collectionName);
+        expect(response.result).to.equal("copper");
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.linsert", async () => {
+        const response = await c8Client.redis.linsert(
+          "list",
+          "AFTER",
+          "copper",
+          "silver",
+          collectionName
+        );
+        expect(response.result).to.equal(4);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.llen", async () => {
+        const response = await c8Client.redis.llen("list", collectionName);
+        expect(response.result).to.equal(4);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lrange", async () => {
+        const response = await c8Client.redis.lrange(
+          "list",
+          0,
+          1,
+          collectionName
+        );
+        expect(response.result).to.eql(["copper", "silver"]);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lmove", async () => {
+        const arrayData1 = ["a", "b", "c"];
+        await c8Client.redis.lpush("testList1", arrayData1, collectionName);
+
+        const arrayData2 = ["x", "y", "z"];
+        await c8Client.redis.lpush("testList2", arrayData2, collectionName);
+
+        const response = await c8Client.redis.lmove(
+          "testList1",
+          "testList2",
+          "RIGHT",
+          "LEFT",
+          collectionName
+        );
+        expect(response.result).to.equal("a");
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.rpush", async () => {
+        const arrayData = ["a", "b", "c", "d", "a", "a"];
+        const response = await c8Client.redis.rpush(
+          "testListPos",
+          arrayData,
+          collectionName
+        );
+        expect(response.result).to.equal(6);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lpos", async () => {
+        const response = await c8Client.redis.lpos(
+          "testListPos",
+          "a",
+          collectionName
+        );
+        expect(response.result).to.equal(0);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lpos2", async () => {
+        const response = await c8Client.redis.lpos(
+          "testListPos",
+          "a",
+          collectionName,
+          0,
+          3
+        );
+        expect(response.result).to.eql([0, 4, 5]);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lpop", async () => {
+        const response = await c8Client.redis.lpop(
+          "testList2",
+          collectionName,
+          1
+        );
+        expect(response.result).to.eql(["a"]);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lpushx", async () => {
+        const arrayData = ["a", "b", "c", "d", "a", "a"];
+        const response = await c8Client.redis.lpushx(
+          "testListPos",
+          arrayData,
+          collectionName
+        );
+        expect(response.result).to.equal(12);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lrem", async () => {
+        const arrayData = ["hello", "hello", "foo", "hello"];
+        await c8Client.redis.rpush("testListLrem", arrayData, collectionName);
+        const response = await c8Client.redis.lrem(
+          "testListLrem",
+          -2,
+          "hello",
+          collectionName
+        );
+        expect(response.result).to.equal(2);
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.lset", async () => {
+        const response = await c8Client.redis.lset(
+          "testListLrem",
+          0,
+          "test",
+          collectionName
+        );
+        expect(response.result).to.equal("OK");
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.trim", async () => {
+        const response = await c8Client.redis.ltrim(
+          "testListLrem",
+          0,
+          0,
+          collectionName
+        );
+        expect(response.result).to.equal("OK");
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.rpop", async () => {
+        const response = await c8Client.redis.rpop(
+          "testListLrem",
+          collectionName
+        );
+        expect(response.result).to.equal("test");
+        expect(response.code).to.equal(200);
+      });
+
+      it("redis.rpoplpush", async () => {
+        const arrayData = ["one", "two", "three"];
+        await c8Client.redis.rpush("myPushList", arrayData, collectionName);
+        const response = await c8Client.redis.rpoplpush(
+          "myPushList",
+          "myOtherPushList",
+          collectionName
+        );
+        expect(response.result).to.equal("three");
+        expect(response.code).to.equal(200);
+      });
+    });
+    describe("test redis hash commands", () => {});
   });
 });
