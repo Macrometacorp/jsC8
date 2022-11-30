b1ba8334c5831525695b95cf40517aa33f13d2db
Dino Lozina <dino.lozina@macrometa.com>
2022-11-30 18:31:13 +0100

    Added first endpoints

--- src/function.ts
+++ src/function.ts
@@ -1,5 +1,12 @@
 import {Config, Connection} from "./connection";
 
+export interface DeployParameters {
+    type?: string;
+    edgeWorkerName: string;
+    queryWorkerName: string;
+    environment?: string;
+}
+
 export class Function {
     protected _connection: Connection;
 
@@ -13,6 +20,39 @@ export class Function {
                 method: "GET",
                 path: "/_api/function",
                 qs: type
+            },
+            res => res.body
+        );
+    }
+
+    deployQueryWorkerToEdgeWorker(
+        parameters: DeployParameters
+        ){
+        return this._connection.request(
+            {
+                method: "GET",
+                path: "/_api/function/generate",
+                qs: parameters
+            },
+            res => res.body
+        );
+    }
+
+    getFuctionWorkerInfo(functionName: string){
+        return this._connection.request(
+            {
+                method: "GET",
+                path: `/_api/function/${functionName}`,
+            },
+            res => res.body
+        );
+    }
+
+    getEdgeWorkerMetadata(){
+        return this._connection.request(
+            {
+                method: "GET",
+                path: "/_api/function/metadata",
             },
             res => res.body
         );
