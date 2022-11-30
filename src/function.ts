aba135f555e6359cbebebf446defe7ccd7749605
Dino Lozina <dino.lozina@macrometa.com>
2022-11-30 19:56:37 +0100

    Added new generate endpoint

--- src/function.ts
+++ src/function.ts
@@ -1,60 +1,57 @@
-import {Config, Connection} from "./connection";
+import { Config, Connection } from "./connection";
 
 export interface DeployParameters {
-    type?: string;
-    edgeWorkerName: string;
-    queryWorkerName: string;
-    environment?: string;
+  type: string;
+  edgeWorkerName: string;
+  queryWorkerName: string;
+  environment: string;
 }
 
 export class Function {
-    protected _connection: Connection;
+  protected _connection: Connection;
 
-    constructor(config?: Config) {
-        this._connection = new Connection(config);
-    }
+  constructor(config?: Config) {
+    this._connection = new Connection(config);
+  }
+  listFunctionWorkers(type: string = "all") {
+    return this._connection.request(
+      {
+        method: "GET",
+        path: "/_api/function",
+        qs: type,
+      },
+      res => res.body
+    );
+  }
 
-    listFunctionWorkers(type: string = "all"){
-        return this._connection.request(
-            {
-                method: "GET",
-                path: "/_api/function",
-                qs: type
-            },
-            res => res.body
-        );
-    }
+  deployQueryWorkerToEdgeWorker(parameters: DeployParameters) {
+    return this._connection.request(
+      {
+        method: "POST",
+        path: "/_api/function/generate",
+        qs: parameters,
+      },
+      res => res.body
+    );
+  }
 
-    deployQueryWorkerToEdgeWorker(
-        parameters: DeployParameters
-        ){
-        return this._connection.request(
-            {
-                method: "GET",
-                path: "/_api/function/generate",
-                qs: parameters
-            },
-            res => res.body
-        );
-    }
+  getFuctionWorkerInfo(functionName: string) {
+    return this._connection.request(
+      {
+        method: "GET",
+        path: `/_api/function/${functionName}`,
+      },
+      res => res.body
+    );
+  }
 
-    getFuctionWorkerInfo(functionName: string){
-        return this._connection.request(
-            {
-                method: "GET",
-                path: `/_api/function/${functionName}`,
-            },
-            res => res.body
-        );
-    }
-
-    getEdgeWorkerMetadata(){
-        return this._connection.request(
-            {
-                method: "GET",
-                path: "/_api/function/metadata",
-            },
-            res => res.body
-        );
-    }
+  getEdgeWorkerMetadata() {
+    return this._connection.request(
+      {
+        method: "GET",
+        path: "/_api/function/metadata",
+      },
+      res => res.body
+    );
+  }
 }
