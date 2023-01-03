7163c6c4a4a7c1f4aa843e523a05bd342d47d69c
Dino Lozina <dino.lozina@live.com>
2023-01-03 19:35:10 +0100

    Corrected manipulating fabrics tests

--- src/test/01-manipulating-fabrics.ts
+++ src/test/01-manipulating-fabrics.ts
@@ -1,8 +1,9 @@
 import { expect } from "chai";
-import { Fabric } from "../jsC8";
+import { C8Client } from "../jsC8";
 import { C8Error } from "../error";
 import { getDCListString } from "../util/helper";
-import { DocumentCollection } from "../collection";
+import { Fabric } from "../jsC8";
+import * as dotenv from "dotenv";
 
 const range = (n: number): number[] => Array.from(Array(n).keys());
 const C8_VERSION = Number(process.env.C8_VERSION || 30400);
@@ -10,111 +11,94 @@ const it2x = C8_VERSION < 30000 ? it : it.skip;
 
 describe("Manipulating fabrics", function() {
   // create fabric takes 11s in a standard cluster
+  dotenv.config();
   this.timeout(60000);
+  let c8Client: C8Client;
 
-  let fabric: Fabric;
-  const testUrl: string =
-    process.env.TEST_C8_URL || "https://test.macrometa.io";
-
-  let dcList: string;
-  beforeEach(async () => {
-    fabric = new Fabric({
-      url: testUrl,
+  before(async () => {
+    c8Client = new C8Client({
+      url: process.env.URL,
+      apiKey: process.env.API_KEY,
+      fabricName: process.env.FABRIC,
       c8Version: C8_VERSION,
     });
-
-    await fabric.login("guest@macrometa.io", "guest");
-    fabric.useTenant("guest");
-
-    const response = await fabric.getAllEdgeLocations();
-    dcList = getDCListString(response);
   });
-  afterEach(() => {
-    fabric.close();
-  });
-  describe("fabric.version", () => {
-    this.beforeAll(async () => {
-      fabric = new Fabric({
-        url: testUrl,
-        c8Version: C8_VERSION,
-      });
 
-      await fabric.login("guest@macrometa.io", "guest");
-      fabric.useTenant("guest");
-
-      const response = await fabric.getAllEdgeLocations();
-      dcList = getDCListString(response);
-    });
-    this.afterAll(() => {
-      fabric.close();
-    });
-    it("should return the version object when no details are required", async () => {
-      const response = await fabric.version();
-      expect(response.server).to.equal("C8DB");
-    });
-    it("should return the version object when details are required", async () => {
-      const response = await fabric.version(true);
-      expect(response.server).to.equal("C8DB");
-      expect(response.details).to.haveOwnProperty("architecture");
-    });
-  });
   describe("fabric.useFabric", () => {
+    it("should return the version object when no details are required", async () => {
+      const response = await c8Client.version();
+      expect(response.server).to.equal("C8DB");
+    });
     it("updates the fabric name", () => {
       const name = "example";
-      expect(fabric.name).to.equal("_system"); // default
-      fabric.useFabric(name);
-      expect((fabric as any)._connection).to.have.property("_fabricName", name);
-      expect(fabric.name).to.equal(name);
+      expect(c8Client.name).to.equal("_system"); // default
+      c8Client.useFabric(name);
+      expect((c8Client as any)._connection).to.have.property(
+        "_fabricName",
+        name
+      );
+      expect(c8Client.name).to.equal(name);
     });
     it("returns itself", () => {
-      const fabric2 = fabric.useFabric("nope");
-      expect(fabric).to.equal(fabric2);
+      const fabric2 = c8Client.useFabric("nope");
+      expect(c8Client).to.equal(fabric2);
     });
   });
   describe("fabric.edgeLocations", () => {
-    this.beforeEach(() => fabric.useFabric("_system"));
+    this.beforeEach(() => c8Client.useFabric("_system"));
     it("gets all the edge locations", async () => {
-      const response = await fabric.getAllEdgeLocations();
+      const response = await c8Client.getAllEdgeLocations();
       expect(Array.isArray(response)).to.be.true;
-      expect(response[0]).to.haveOwnProperty("_id");
+      expect(response[0]).to.haveOwnProperty("_key");
+      expect(response[0]).to.haveOwnProperty("host");
+      expect(response[0]).to.haveOwnProperty("local");
       expect(response[0]).to.haveOwnProperty("name");
+      expect(response[0]).to.haveOwnProperty("spot_region");
+      expect(response[0]).to.haveOwnProperty("status");
+      expect(response[0]).to.haveOwnProperty("tags");
       expect(response[0]).to.haveOwnProperty("tags");
       expect(response.length).to.be.greaterThan(0);
     });
     it("gets local edge location", async () => {
-      const response = await fabric.getLocalEdgeLocation();
+      const response = await c8Client.getLocalEdgeLocation();
       expect(Array.isArray(response)).to.be.false;
-      expect(response).to.haveOwnProperty("_id");
+      expect(response).to.haveOwnProperty("_key");
+      expect(response).to.haveOwnProperty("host");
+      expect(response).to.haveOwnProperty("local");
       expect(response).to.haveOwnProperty("name");
+      expect(response).to.haveOwnProperty("spot_region");
+      expect(response).to.haveOwnProperty("status");
+      expect(response).to.haveOwnProperty("tags");
       expect(response).to.haveOwnProperty("tags");
     });
   });
   describe("fabric.createFabric", () => {
     let name = `testfabric${Date.now()}`;
     afterEach(async () => {
-      fabric.useFabric("_system");
-      await fabric.dropFabric(name);
+      c8Client.useFabric("_system");
+      await c8Client.dropFabric(name);
     });
     it("creates a fabric with the given name", async () => {
-      await fabric.createFabric(name, ["root"], {
+      const response = await c8Client.getAllEdgeLocations();
+      let dcList: string = getDCListString(response);
+      await c8Client.createFabric(name, ["root"], {
         dcList: dcList,
       });
-      fabric.useFabric(name);
-      const info = await fabric.get();
+      c8Client.useFabric(name);
+      const info = await c8Client.get();
       expect(info.name).to.equal(name);
     });
-    it("adds the given users to the fabric");
   });
   describe("fabric.get", () => {
     it("fetches the fabric description if the fabric exists", async () => {
-      const info = await fabric.get();
-      expect(info.name).to.equal(fabric.name);
-      expect(fabric.name).to.equal("_system");
+      const info = await c8Client.get();
+      expect(info.name).to.equal(c8Client.name);
+      expect(c8Client.name).to.equal("_system");
     });
     it("fails if the fabric does not exist", async () => {
-      fabric.useFabric("__does_not_exist__");
+      c8Client.useFabric("__does_not_exist__");
       try {
-        await fabric.get();
+        await c8Client.get();
       } catch (e) {
         expect(e).to.be.an.instanceof(C8Error);
         return;
@@ -124,14 +108,14 @@ describe("Manipulating fabrics", function() {
   });
   describe("fabric.listFabrics", () => {
     it("returns a list of all fabrics", async () => {
-      const fabrics = await fabric.listFabrics();
+      const fabrics = await c8Client.listFabrics();
       expect(fabrics).to.be.an.instanceof(Array);
       expect(fabrics.indexOf("_system")).to.be.greaterThan(-1);
     });
   });
   describe("fabric.listUserFabrics", () => {
     it("returns a list of fabrics accessible to the active user", async () => {
-      const fabrics = await fabric.listUserFabrics();
+      const fabrics = await c8Client.listUserFabrics();
       expect(fabrics).to.be.an.instanceof(Array);
       expect(fabrics[0].name).to.equal("_system");
     });
@@ -139,12 +123,14 @@ describe("Manipulating fabrics", function() {
   describe("fabric.dropFabric", () => {
     let name = `testfabric${Date.now()}`;
     beforeEach(async () => {
-      await fabric.createFabric(name, ["root"], {
+      const response = await c8Client.getAllEdgeLocations();
+      let dcList: string = getDCListString(response);
+      await c8Client.createFabric(name, ["root"], {
         dcList: dcList,
       });
     });
     it("deletes the given fabric from the server", async () => {
-      await fabric.dropFabric(name);
+      await c8Client.dropFabric(name);
       let temp = new Fabric().useFabric(name);
       try {
         await temp.get();
@@ -156,38 +142,42 @@ describe("Manipulating fabrics", function() {
       expect.fail("should not succeed");
     });
   });
+  // We need to be sure that user can create system collection
+  // C8Error: tenant cannot create system collection
   describe("fabric.truncate", () => {
     let name = `testfabric${Date.now()}`;
     let nonSystemCollections = range(4).map(i => `c${i}${Date.now()}`);
     let systemCollections = range(4).map(i => `c${i}${Date.now()}${i}`);
     beforeEach(async () => {
-      await fabric.createFabric(name, ["root"], {
+      const response = await c8Client.getAllEdgeLocations();
+      let dcList: string = getDCListString(response);
+      await c8Client.createFabric(name, ["root"], {
         dcList: dcList,
       });
-      fabric.useFabric(name);
+      c8Client.useFabric(name);
       await Promise.all([
         ...nonSystemCollections.map(async name => {
-          let collection = fabric.collection(name);
+          let collection = c8Client.collection(name);
           await collection.create();
           await collection.save({ _key: "example" });
         }),
         ...systemCollections.map(async name => {
-          let collection = fabric.collection(name);
+          let collection = c8Client.collection(name);
           await collection.create({ isSystem: true });
           await collection.save({ _key: "example" });
         }),
       ]);
     });
     afterEach(async () => {
-      fabric.useFabric("_system");
-      await fabric.dropFabric(name);
+      c8Client.useFabric("_system");
+      await c8Client.dropFabric(name);
     });
     it("removes all documents from all non-system collections in the fabric", async () => {
-      await fabric.truncate();
+      await c8Client.truncate();
       await Promise.all([
         ...nonSystemCollections.map(async name => {
           try {
-            await fabric.collection(name).document("example");
+            await c8Client.collection(name).document("example");
             expect.fail("Expected document to be destroyed");
           } catch (e) {
             const message = String(e);
@@ -201,7 +191,7 @@ describe("Manipulating fabrics", function() {
         }),
         ...systemCollections.map(async name => {
           try {
-            await fabric.collection(name).document("example");
+            await c8Client.collection(name).document("example");
             expect.fail("Expected document to be destroyed");
           } catch (e) {
             const message = String(e);
@@ -218,12 +208,12 @@ describe("Manipulating fabrics", function() {
     it2x(
       "additionally truncates system collections if explicitly passed false",
       async () => {
-        await fabric.truncate(false);
+        await c8Client.truncate(false);
         await Promise.all(
           nonSystemCollections.map(async name => {
             let doc;
             try {
-              doc = await fabric.collection(name).document("example");
+              doc = await c8Client.collection(name).document("example");
             } catch (e) {
               expect(e).to.be.an.instanceof(C8Error);
               return;
@@ -236,12 +226,14 @@ describe("Manipulating fabrics", function() {
   });
   describe("fabric.validateQuery", () => {
     it("should validate correct query", async () => {
-      const response = await fabric.validateQuery("for doc in docs return doc");
+      const response = await c8Client.validateQuery(
+        "for doc in docs return doc"
+      );
       expect(response.error).to.be.false;
     });
     it("should validate incorrect query", async () => {
       try {
-        const response = await fabric.validateQuery(
+        const response = await c8Client.validateQuery(
           "forrrr doc in docs return doc"
         );
         expect(response.error).to.be.true;
@@ -252,35 +244,31 @@ describe("Manipulating fabrics", function() {
   });
   describe("fabric.explainQuery", () => {
     const collectionName = `testColl${Date.now().toString()}`;
-    let collection: DocumentCollection;
+
     this.beforeAll(async () => {
-      const fabric2 = new Fabric({
-        url: testUrl,
-      });
-      await fabric2.login("guest@macrometa.io", "guest");
-      fabric2.useTenant("guest");
-      collection = fabric2.collection(collectionName);
-      await collection.create();
+      await c8Client.createCollection(collectionName);
     });
     this.afterAll(async () => {
-      await collection.drop();
+      await c8Client.deleteCollection(collectionName);
     });
     it("should explain query", async () => {
       const queryObject = {
         query: `for doc in ${collectionName} return doc`,
         bindVars: {},
       };
-      const response = await fabric.explainQuery(queryObject);
+      const response = await c8Client.explainQuery(queryObject);
       expect(response.error).to.be.false;
     });
   });
-  describe("fabric.getCurrentQueries", () => {
-    it("should get currently running queries");
-  });
-  describe("fabric.clearSlowQueries", () => {
-    it("should clear slow queries");
-  });
-  describe("fabric.terminateRunningQuery", () => {
-    it("should terminate running query");
-  });
 });
+
+//   describe("fabric.getCurrentQueries", () => {
+//     it("should get currently running queries");
+//   });
+//   describe("fabric.clearSlowQueries", () => {
+//     it("should clear slow queries");
+//   });
+//   describe("fabric.terminateRunningQuery", () => {
+//     it("should terminate running query");
+//   });
+// });
