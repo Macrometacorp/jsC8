b49138952d0c7c34619024cbdbe52e48c9971ec1
dwivedianurag <anurag.dwivedi@macrometa.com>
2023-03-18 03:31:41 +0530

    add internal url support

--- src/connection.ts
+++ src/connection.ts
@@ -164,8 +164,10 @@ export class Connection {
         : [config.url]
       : ["https://play.macrometa.io/"];
     const apiUrls = urls.map((url) => {
-      // return `https://api-${url.split("https://")[1]}`;
-      return `http://c8db-coord-svc.c8:8529`;
+    if(  url==='http://c8db-coord-svc.c8:8529')
+    {return `http://c8db-coord-svc.c8:8529`}
+    else
+    {return `https://api-${url.split("https://")[1]}`;}
     });
     this.addToHostList(apiUrls);
 
